04ebc6baf1657fa7dd8db6fc8d95701202b271f3
sabrina-mi <sabrinami2000@gmail.com>
2022-08-19 17:40:43 -0400

    add scripts to submit GEMMA jobs

---
+++ code/Pl_GEMMA_BSLMM.sh
@@ -0,0 +1,15 @@
+DIR=/gpfs/data/im-lab/nas40t2/natasha/rat_genomics/GEMMA/Pl
+mv $DIR/output $DIR/GRMs
+mkdir completed_jobs
+cd /gpfs/data/im-lab/nas40t2/Github/badger
+/gpfs/data/im-lab/nas40t2/lab_software/miniconda/envs/ukb_env/bin/python src/badger.py \
+-yaml_configuration_file $DIR/GEMMA_submission.yaml \
+-parsimony 9
+cd $DIR
+for job in jobs/gemma*; do
+    if  (($(qstat | wc -l) > 10000)); then
+        sleep 10m
+    fi
+    qsub $job
+    mv $job completed_jobs
+done
